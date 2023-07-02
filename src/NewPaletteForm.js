a4c08188c356e5cee13826727592edff9960a68d
Alesia Haxhi <alesiahaxhi@outlook.com>
2023-07-03 01:36:17 +0200

    add form validator

--- src/NewPaletteForm.js
+++ src/NewPaletteForm.js
@@ -1,5 +1,7 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+
 import { styled, useTheme } from "@mui/material/styles";
+
 import Button from "@mui/material/Button";
 import Box from "@mui/material/Box";
 import Drawer from "@mui/material/Drawer";
@@ -12,6 +14,7 @@ import IconButton from "@mui/material/IconButton";
 import MenuIcon from "@mui/icons-material/Menu";
 import ChevronLeftIcon from "@mui/icons-material/ChevronLeft";
 import ChevronRightIcon from "@mui/icons-material/ChevronRight";
+import TextField from "@mui/material/TextField";
 
 import { ChromePicker } from "react-color";
 import DragColorBox from "./DragColorBox";
@@ -68,7 +71,10 @@ const NewPaletteForm = () => {
   const theme = useTheme();
   const [open, setOpen] = React.useState(false);
   const [color, setColor] = useState("#000000");
-  const [colors, setColors] = useState(["purple", "cyan"]);
+  const [colors, setColors] = useState([]);
+  const [colorName, setColorName] = useState("");
+  const [error, setError] = useState(false);
+  const [touched, setTouched] = useState(false);
 
   const handleDrawerOpen = () => {
     setOpen(true);
@@ -82,8 +88,59 @@ const NewPaletteForm = () => {
     setColor(newColor.hex);
   };
 
+  const handleBlur = () => {
+    setTouched(true);
+  };
+
   const createColor = () => {
-    setColors([...colors, color]);
+    const newColor = {
+      color: color,
+      name: colorName,
+    };
+    setColors([...colors, newColor]);
+    setColorName("");
+  };
+
+  const validateColor = (name, newColor) => {
+    const isDuplicateName = colors.some(
+      (color) => color.name.toLowerCase() === name.toLowerCase()
+    );
+    const isDuplicateColor = colors.some(
+      (color) => color.color.toLowerCase() === newColor.toLowerCase()
+    );
+
+    return isDuplicateName || isDuplicateColor;
+  };
+
+  useEffect(() => {
+    const isInvalid = validateColor(colorName, color);
+    setError({
+      isDuplicateName: isInvalid && colors.some((c) => c.name === colorName),
+      isDuplicateColor: isInvalid && colors.some((c) => c.color === color),
+      isEmpty: touched && colorName.trim() === "",
+    });
+  }, [colorName, color, colors, touched]);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (colorName === "") {
+      setError({
+        isDuplicateName: false,
+        isDuplicateColor: false,
+        isEmpty: true,
+      });
+      return;
+    }
+    const isInvalid = validateColor(colorName, color);
+    if (isInvalid) {
+      setError({
+        isDuplicateName: false,
+        isDuplicateColor: true,
+        isEmpty: false,
+      });
+      return;
+    }
+    createColor();
   };
 
   return (
@@ -136,19 +193,43 @@ const NewPaletteForm = () => {
           <Button variant="contained">Random Color</Button>
         </div>
         <ChromePicker color={color} onChange={handleChange} />
-        <Button
-          variant="contained"
-          onClick={createColor}
-          style={{ backgroundColor: color }}
-        >
-          ADD COLOR
-        </Button>
+        <form onSubmit={handleSubmit}>
+          <TextField
+            required
+            value={colorName}
+            id="standard-basic"
+            label="Color Name"
+            variant="standard"
+            onChange={(e) => setColorName(e.target.value)}
+            onBlur={handleBlur}
+            error={
+              error &&
+              (error.isEmpty || error.isDuplicateName || error.isDuplicateColor)
+            }
+            helperText={
+              error.isEmpty && touched && colorName.trim() === ""
+                ? "Color name cannot be empty"
+                : error.isDuplicateName
+                ? "Oops! That name's taken!"
+                : error.isDuplicateColor
+                ? "Color already in use!"
+                : ""
+            }
+          />
+          <Button
+            variant="contained"
+            type="submit"
+            style={{ backgroundColor: color }}
+          >
+            ADD COLOR
+          </Button>
+        </form>
       </Drawer>
       <Main open={open}>
         <DrawerHeader />
 
-        {colors.map((color) => (
-          <DragColorBox color={color} />
+        {colors.map((color, index) => (
+          <DragColorBox key={index} color={color.color} name={color.name} />
         ))}
       </Main>
     </Box>
