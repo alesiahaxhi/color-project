538a5709e46f60e42704f427df013732bf9583ac
Alesia Haxhi <alesiahaxhi@outlook.com>
2023-07-10 16:16:59 +0200

    add DragColorList.js

---
+++ src/DragColorList.js
@@ -0,0 +1,26 @@
+import DragColorBox from "./DragColorBox";
+import { ReactSortable } from "react-sortablejs";
+
+export default function DragColorList(props) {
+  const { colors, setColors, removeColor } = props;
+
+  return (
+    <ReactSortable
+      tag="div"
+      list={colors} // Ensure that colors is an array
+      setList={setColors}
+      style={{ height: "100%" }}
+    >
+      {colors &&
+        colors.map((color, i) => (
+          <DragColorBox
+            index={i}
+            key={color.name}
+            color={color.color}
+            name={color.name}
+            handleClick={() => removeColor(color.name)}
+          />
+        ))}
+    </ReactSortable>
+  );
+}
